f109e3805b518d4465fd1d26e54761c7a24c1fb3
sai-nanami <muchammadsyakiruddin@gmail.com>
2026-03-06 14:33:48 +0700

    feat: update alert message

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Git Workflow Simulation Script
 
 function showMessage() {
-    alert("Praktikum Git Workflow berjalan.");
+    alert("Praktikum Git Workflow berhasil dilakukan.");
 }
 
 console.log("Git Workflow Simulation loaded.");
